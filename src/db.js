fe4324aa99e8522e5ded896c116cac3d7cf4ca8e
Cristian Gutierrez <cdgutierrez456@gmail.com>
2023-11-06 18:14:13 -0500

    Agregando libreria dotenv para variables de entorno, separando codigo de app en un archivo aparte, creando archivo config.js para obtener las variables de entorno...

--- src/db.js
+++ src/db.js
@@ -1,10 +1,17 @@
 import { createPool } from "mysql2/promise";
+import { 
+  DB_PORT,
+  DB_HOST,
+  DB_PASS,
+  DB_USER,
+  DB_PORT
+} from './config.js'
 
 export const pool = createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-  port: 3306,
-  database: 'companydb'
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASS,
+  port: DB_PORT,
+  database: DB_DATABASE
 })
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-import express from "express";
-import employeesRoutes from "./routes/employees.routes.js";
-import indexRoutes from './routes/index.routes.js';
+import app from './app.js'
+import { PORT } from './config.js'
 
-const app = express()
-
-app.use(express.json())
-
-app.use('/api', indexRoutes)
-app.use('/api', employeesRoutes)
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'Endpoint not found'
-  })
-})
-
-app.listen(3000)
-console.log('Server running in port 3000')
+app.listen(PORT)
+console.log(`Server running in port ${PORT}`)
